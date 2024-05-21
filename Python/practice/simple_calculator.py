40c1b1734221afcd743ac103bdc7cd814e7eda29
Zakhar Adeev <153710848+zackneiro@users.noreply.github.com>
2024-05-21 09:22:01 +0000

    Add some error check

--- Python/practice/simple_calculator.py
+++ Python/practice/simple_calculator.py
@@ -1,11 +1,18 @@
 def main():
-    x = int(input("x: "))
-    y = int(input("y: "))
+    x = get_int("x: ")
+    y = get_int("y: ")
     option = input("Choose your operation: "
                    "+, -, /, %, *, //: ")
     operations(option, x , y)
     return
 
+def get_int(promt):
+    while True:
+        try:
+            return int(input(promt))
+        except:
+            ValueError
+            print("Not an integer.")
 
 def operations(option, x, y):
     if option == '+':

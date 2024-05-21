b6b6b67d520a5561ab5e0d1fa59d919ebdd37d9b
Zakhar Adeev <153710848+zackneiro@users.noreply.github.com>
2024-05-21 09:16:05 +0000

    Declare new function to improve clear and readability

--- Python/practice/simple_calculator.py
+++ Python/practice/simple_calculator.py
@@ -1,18 +1,25 @@
-x = int(input("x: "))
-y = int(input("y: "))
+def main():
+    x = int(input("x: "))
+    y = int(input("y: "))
+    option = input("Choose your operation: "
+                   "+, -, /, %, *, //: ")
+    operations(option, x , y)
+    return
 
 
-option = input("Choose your operation: "
-               "+, -, /, %, *, //: ")
-if option == '+':
-    print(x + y)
-elif option == '-':
-    print(x - y)
-elif option == '/':
-    print(x / y)
-elif option == '%':
-    print(x % y)
-elif option == '*':
-    print(x * y)
-elif option == '//':
-    print(x // y)
+def operations(option, x, y):
+    if option == '+':
+        print(x + y)
+    elif option == '-':
+        print(x - y)
+    elif option == '/':
+        print(x / y)
+    elif option == '%':
+        print(x % y)
+    elif option == '*':
+        print(x * y)
+    elif option == '//':
+        print(x // y)
+
+
+main()

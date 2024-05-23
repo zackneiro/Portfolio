7355d65cf02932df5e79961dc76cd432a647a6d7
Zakhar Adeev <153710848+zackneiro@users.noreply.github.com>
2024-05-23 13:03:30 +0000

    Corrected the mistake in factorial_test.py

--- Python/practice/factorial/factorial_test.py
+++ Python/practice/factorial/factorial_test.py
@@ -2,7 +2,7 @@ import pytest
 from Python.practice.factorial.factorial_calc import factorial_calculation
 
 def test_factorial_calculation():
-    assert factorial_calculation(4) == 4
+    assert factorial_calculation(4) == 24
     assert factorial_calculation(5) == 120
     assert factorial_calculation(10) == 3628800
     assert factorial_calculation(0) == 1 # test case for 0

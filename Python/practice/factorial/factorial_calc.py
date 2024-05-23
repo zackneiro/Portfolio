af21fa8cd94a289001b5acc1ec8361ea786dcb3a
Zakhar Adeev <153710848+zackneiro@users.noreply.github.com>
2024-05-23 13:16:16 +0000

    Added more clear comments to fractorial scripts

--- Python/practice/factorial/factorial_calc.py
+++ Python/practice/factorial/factorial_calc.py
@@ -5,26 +5,40 @@ import os
 def main():
     """
     Main function to perform factorial calculations.
+    Depending on the environment, it either takes user input or reads from environment variables.
     """
-    print("Starting the compare script...")  # Debug print
+    print("Starting the factorial calculation script...")  # Debug print
+
+    # Check if the script is running in a CI environment
     if os.getenv('CI', 'false').lower() == 'true':
         n = int(os.getenv('INPUT', 0))
     else:
-
+        # Prompt the user to enter an integer
         n = get_int("Please enter an integer: ")
 
+    # Calculate the factorial of the input number
     result = factorial_calculation(n)
+
+    # Print the result
     print(f"Factorial of number {n} is {result}")
 
 
 def factorial_calculation(n):
     """
-    Faction to perform factorial calculations by making a recurseive call
-    """
+    Function to perform factorial calculation using recursion.
     
+    Args:
+        n (int): The integer for which the factorial is to be calculated.
+        
+    Returns:
+        int: The factorial of the input number. Returns 1 for non-positive integers.
+    """
+
+    # Base case: factorial of 0 or negative numbers is 1
     if n <= 0:
         return 1
     
+    # Recursive case: n * factorial of (n-1)
     return n * factorial_calculation(n - 1)
 
 
--- Python/practice/factorial/factorial_test.py
+++ Python/practice/factorial/factorial_test.py
@@ -1,9 +1,23 @@
 import pytest
 from Python.practice.factorial.factorial_calc import factorial_calculation
 
+
 def test_factorial_calculation():
-    assert factorial_calculation(4) == 24
-    assert factorial_calculation(5) == 120
-    assert factorial_calculation(10) == 3628800
-    assert factorial_calculation(0) == 1 # test case for 0
-    assert factorial_calculation(-1) == 1 # test case for negative number
+    """
+    Function to test the factorial calculations with different arguments.
+    This test function covers various cases including positive integers, zero, and negative integers.
+    """
+    # Test case for positive integer 4
+    assert factorial_calculation(4) == 24, "Test failed for input 4"
+    
+    # Test case for positive integer 5
+    assert factorial_calculation(5) == 120, "Test failed for input 5"
+    
+    # Test case for positive integer 10
+    assert factorial_calculation(10) == 3628800, "Test failed for input 10"
+    
+    # Test case for 0
+    assert factorial_calculation(0) == 1, "Test failed for input 0"
+    
+    # Test case for negative number
+    assert factorial_calculation(-1) == 1, "Test failed for input -1"

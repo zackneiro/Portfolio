fcc00e8cdba445eec47e162e046d7c5210659761
Zakhar Adeev <153710848+zackneiro@users.noreply.github.com>
2024-05-23 08:54:07 +0000

    Included the root directory of my project

--- Python/practice/compare.py
+++ Python/practice/compare.py
@@ -1,1 +1,34 @@
-import simple_calculator
+from .simple_calculator import get_int
+
+def main():
+    """
+    Main function to excute the compare operations.
+    """
+    x = get_int("Enter the first number: ", min_value=-1000000, max_value=1000000)
+    y = get_int("Enter the second number: ", min_value=-1000000, max_value=1000000)
+
+    compare(x, y)
+
+
+    def compare(x, y):
+        """
+        Function to compare two integers and print the result.
+        """
+        if x < y:
+            print(f"{x} < {y}")
+        elif x > y:
+            print(f"{x} > {y}")
+        elif x == y:
+            print(f"{x} = {y}")
+        else:
+            print("Unxpected error")
+
+        
+
+
+
+
+
+
+    if __name__ == '__main__':
+        main()

e6b432790b49f2aca24a1daf7225406dc9039c85
Zakhar Adeev <153710848+zackneiro@users.noreply.github.com>
2024-05-25 14:37:52 +0000

    Added more clear comments to expalin the logic

--- Python/practice/compare/test_compare.py
+++ Python/practice/compare/test_compare.py
@@ -4,6 +4,8 @@ from Python.practice.compare.compare import get_input, is_even_or_odd
 def test_is_even_or_odd():
     """
     Function to test the is_even_or_odd function.
+    This function checks if the uis_even_or_odd function correctly identifies
+    whether given numbers are even or odd.
     """
     assert is_even_or_odd(4, 7) == "4 is even and 7 is odd"
     assert is_even_or_odd(2, 2) == "2 is even and 2 is even"
@@ -13,6 +15,7 @@ def test_is_even_or_odd():
 def clear_env(monkeypatch):
     """
     Fixture to clear environment variables for testing.
+    This fixture ensures the the CI- realted enviroment variables are cleared before each test.
     """
     monkeypatch.delenv('CI', raising=False)
     monkeypatch.delenv('INPUT1', raising=False)
@@ -21,6 +24,8 @@ def clear_env(monkeypatch):
 def test_get_input(monkeypatch, clear_env):
     """
     Function to test the get_input function.
+    This test mocks the input function to simulate the user input and verifies 
+    that get_input correctly return the expected values.
     """
     def mock_input(prompt):
         if "first" in prompt:
@@ -37,6 +42,8 @@ def test_get_input(monkeypatch, clear_env):
 def test_get_input_with_env(monkeypatch):
     """
     Test get_input function when CI environment variable is set.
+    This test sets the CI-related environment variables and verifies that 
+    get_input correctly reads these values and returns them.
     """
     monkeypatch.setenv('CI', 'true')
     monkeypatch.setenv('INPUT1', '4')
@@ -46,6 +53,8 @@ def test_get_input_with_env(monkeypatch):
 def test_get_input_without_env(monkeypatch, clear_env):
     """
     Test get_input function when CI environment variable is not set.
+    This test mocks the input function to simulate the user input and verifies
+    that get_input correctly return the expected values.
     """
     def mock_input(prompt):
         if "first" in prompt:

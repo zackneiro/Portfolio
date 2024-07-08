0b8ee6b7934d83a7162e4ea62decb4b39f5e3de4
Zakhar Adeev <153710848+zackneiro@users.noreply.github.com>
2024-07-08 15:40:51 +0000

    Update code structure and organize test images
    Moved test images to a dedicated directory to keep the project clean.
    Improved code structure for better readability and maintainability.
    Added input validation for file paths.
    
    - Moved test images to 'images' directory
    - Updated code structure
    - Enhanced readability
    - Added path input validation

--- Python/captcha_reader/captcha_reader.py
+++ Python/captcha_reader/captcha_reader.py
@@ -1,52 +1,42 @@
-import os, time
+import time
 import cv2
-import matplotlib.pyplot as plt
 import numpy as np
 import pytesseract
 
+
 def main():
+    # read file and store it in 'img'
+    img = load_image()
+
     # Sart point of timing
     start_time = time.perf_counter()
 
-    # read file to and tor it in'img'
-    img = cv2.imread('Python/captcha_reader/1.png')
-
-    # Check that image read correctly
-    if img is None:
-        print(f"Error: Image not loaded. Check the file path: {'Python/captcha_reader/1.png'}")
-        return
-    
     # Preprocess functions for an easier symbol detection
     img = preprocess_image(img)
+
+    #Save processed image to validate it 
+    save_processed_image(img)
 
     # Print symbols from text
     print_text(img)
 
-    # Save the processed image
-    processed_image_path = 'Python/captcha_reader/processed_image.png'
-    cv2.imwrite(processed_image_path, img)
-    print(f"Processed image saved as {processed_image_path}")
-
-    # Display the processed image for verification
-    plt.imshow(img, cmap='gray')
-    plt.title('Processed Image')
-    plt.show()
-
     # END timimng and counting the total execution time of run
-    end_time  = time.perf_counter()
-    execution_time = end_time - start_time
-    print(f"Execution time: {execution_time:.8f} seconds")
+    end_time = time.perf_counter()
+    count_execution_time(start_time, end_time)
 
+
+def get_image_path():
+   try:
+    return input("Please, prvoide code with a path to the image: ")
+   except:
+       ValueError
 
 def preprocess_image(image):
-    # Convert to grayscale
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    
     # Apply thresholding
-    _, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    _, thresh = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
     
     # Denoise
-    denoised = cv2.fastNlMeansDenoising(thresh, None, 10, 7, 21)
+    denoised = cv2.medianBlur(thresh, 3)
     
     # Dilation and erosion to remove noise
     kernel = np.ones((1, 1), np.uint8)
@@ -55,9 +45,31 @@ def preprocess_image(image):
     
     return eroded
 
-def print_text(boxed_image):
+
+def print_text(image):
     custom_config = r'--oem 3 --psm 6 -l eng'
-    print(pytesseract.image_to_string(boxed_image, config=custom_config))
+    print(pytesseract.image_to_string(image, config=custom_config))
+
+
+def load_image():
+    image_path = get_image_path()
+    img = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+
+    # Check that image read correctly
+    if img is None:
+        print(f"Error: Image not loaded. Check the file path: {image_path}")
+        exit(1)
+    else:
+        return img
+    
+
+def save_processed_image(image):
+    print("Image write:", "Succedded" if cv2.imwrite('Python/captcha_reader/processed_image.png', image) else "Failed")
+    
+
+def count_execution_time(start_time, end_time):
+    print(f"Execution time: {end_time - start_time:.8f} seconds")
+
 
 if __name__ == "__main__":
     main()

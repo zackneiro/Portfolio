a81cf0b4faf1b2bf580b7eab792eaf8951218993
Zakhar Adeev <153710848+zackneiro@users.noreply.github.com>
2024-07-08 06:24:12 +0000

    Add functions to improve OCR accuracy
    Added a new function `get_boxes` to draw boxes around detected words
    in the text for easier detection. Also added a function to print
    extracted symbols from the text image using custom configuration
    with `image_to_string`.
    
    - Added `get_boxes` function
    - Added `print_text` function to extract and print symbols from the text image

--- Python/captcha_reader/captcha_reader.py
+++ Python/captcha_reader/captcha_reader.py
@@ -2,23 +2,33 @@ import os, time
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
+import pytesseract
+from pytesseract import Output
 
 def main():
     # Sart point of timing
     start_time = time.perf_counter()
 
     # read file to and tor it in'img'
-    img = cv2.imread('Python/captcha_reader/25egp.png')
+    img = cv2.imread('Python/captcha_reader/1.png')
 
 
     # Check that image read correctly
     if img is None:
-        print(f"Error: Image not loaded. Check the file path: {'Python/captcha_reader/25egp.png'}")
+        print(f"Error: Image not loaded. Check the file path: {'Python/captcha_reader/1.png'}")
         return
     
     # Preprocess functions for an easier symbol detection
     img = get_grayscale(img) 
     img = thresholding(img)
+    #img = noise_removal(img)
+    #img = morphological_transform(img)
+    #img = remove_lines(img)
+    # Get boxes aroug the characters
+    img = get_boxes(img, 'Python/captcha_reader/boxed_image.png')
+
+    # Print symbols from captcha
+    print_text(img)
 
 
     # Save the processed image
@@ -48,6 +58,53 @@ def noise_removal(image):
 # Thresholding
 def thresholding(image):
     return cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+
+# Morphological transformations
+def morphological_transform(image):
+    kernel = np.ones((2,2), np.uint8)
+    image = cv2.dilate(image, kernel, iterations=1)
+    image = cv2.erode(image, kernel, iterations=1)
+    return image
+
+
+def get_boxes(image, save_path=None):
+    d = pytesseract.image_to_data(image, output_type=Output.DICT)
+    n_boxes = len(d['text'])
+    for i in range(n_boxes):
+        if int(d['conf'][i]) > 60:
+            (x, y, w, h) = (d['left'][i], d['top'][i], d['width'][i], d['height'][i])
+            image = cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    if save_path is not None:
+        saved = cv2.imwrite(save_path, image)
+        if saved:
+            print(f"Image successfully saved as {save_path}")
+        else:
+            print(f"Failed to save image as {save_path}")
+                  
+    return image
+
+# Remove lines
+def remove_lines(image):
+    # Detect lines in the image using Hough Transform
+    edges = cv2.Canny(image, 50, 150, apertureSize=3)
+    lines = cv2.HoughLines(edges, 1, np.pi / 180, 200)
+    
+    if lines is not None:
+        for rho, theta in lines[:,0]:
+            a = np.cos(theta)
+            b = np.sin(theta)
+            x0 = a * rho
+            y0 = b * rho
+            x1 = int(x0 + 1000 * (-b))
+            y1 = int(y0 + 1000 * (a))
+            x2 = int(x0 - 1000 * (-b))
+            y2 = int(y0 - 1000 * (a))
+            cv2.line(image, (x1, y1), (x2, y2), (0, 0, 0), 2)
+    return image
+
+def print_text(boxed_image):
+    custom_config = r'-l eng --psm 1 '
+    print(pytesseract.image_to_string(boxed_image, config=custom_config))
 
 if __name__ == "__main__":
     main()
